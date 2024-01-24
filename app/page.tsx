e72ca3d95c1471f7eebbffb84707e0727bedc6a4
Yairys Aristigueta <yayi.arist.hz@gmail.com>
2024-01-24 15:27:00 -0300

    filters, SearchBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,38 @@
-import { Hero } from '@/components'
+import { Hero, SearchBar, CustomFilter } from '@/components'
 import Image from 'next/image'
+import { fuels, yearsOfProduction } from '@/constants';
+import { fetchCars } from '@/utils';
+
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
+
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero/>
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+
+          <div className='home__filter-container'>
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
+          </div>
+        </div>
+        </div>
+      
     </main>
   )
 }
